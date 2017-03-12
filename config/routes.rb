d0ec9a3d0b34081959e9710c40a1603603841348
adriennhem <adrien.nhem@gmail.com>
2017-03-12 17:52:50 +0100

    Add subdomains

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,29 @@
+class SubdomainPresent
+  def self.matches?(request)
+    request.subdomain.present? && request.subdomain != "www"
+  end
+end
+class SubdomainBlank
+  def self.matches?(request)
+    request.subdomain().blank?  || request.subdomain == "www"
+  end
+end
+
+
 Rails.application.routes.draw do
-	root 'static_pages#home'
-  get '/contact', to: 'static_pages#contact'
-  get '/help', to: 'static_pages#help'
-  get '/about', to: 'static_pages#about'
-  get '/signup', to: 'users#new'
-  resources :users
+	
+	constraints(SubdomainPresent) do
+    devise_for :users
+    resources :users, only: :index
+  end
+  
+  constraints(SubdomainBlank) do
+    root 'static_pages#home'
+    resources :accounts, only: [:new, :create]
+    get '/contact', to: 'static_pages#contact'
+	  get '/help', to: 'static_pages#help'
+	  get '/about', to: 'static_pages#about'
+	  get '/signup', to: 'users#new'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
