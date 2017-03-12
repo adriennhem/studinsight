93d29d621a0618606c190b4261c247c259455e70
adriennhem <adrien.nhem@gmail.com>
2017-03-12 23:08:21 +0100

    Fix apartment

--- lib/rescued_apartment_middleware.rb
+++ lib/rescued_apartment_middleware.rb
@@ -1,8 +1,10 @@
-class RescuedApartmentMiddleware < ::Apartment::Elevators::Subdomain
-  def call(env)
+module RescuedApartmentMiddleware
+  def call(*args)
+    begin
       super
-    rescue ::Apartment::TenantNotFound
-	  # raise ActionController::RoutingError.new('Not Found')
-      return [404, {"Content-Type" => "text/html"}, ["Not Found"]]
- end
+    rescue Apartment::TenantNotFound
+      Rails.logger.error "ERROR: Apartment Tenant not found: #{Apartment::Tenant.current.inspect}"
+      return [404, {"Content-Type" => "text/html"}, ["#{File.read(Rails.root.to_s + '/public/404.html')}"] ]
+    end
+  end
 end
