20abd508c2d277d3297105a865b46309a6f665ae
adriennhem <adrien.nhem@gmail.com>
2017-03-12 19:57:53 +0100

    test fix rack

--- lib/rescued_apartment_middleware.rb
+++ lib/rescued_apartment_middleware.rb
@@ -2,7 +2,7 @@ class RescuedApartmentMiddleware < ::Apartment::Elevators::Subdomain
   def call(env)
       super
     rescue ::Apartment::TenantNotFound
-	  raise ActionController::RoutingError.new('Not Found')
-      # return [404, {"Content-Type" => "text/html"}, ["Not Found"]]
+	  # raise ActionController::RoutingError.new('Not Found')
+      return [404, {"Content-Type" => "text/html"}, ["Not Found"]]
  end
 end
